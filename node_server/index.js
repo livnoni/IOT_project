6ca478261786a6632e5ed865a83cf2aebc7e6f45
livnoni <yehuda.livnoni@gmail.com>
2018-05-14 15:34:20 +0300

    added web site that show the text

--- node_server/index.js
+++ node_server/index.js
@@ -1,14 +1,35 @@
-var express = require("express");
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
 var bodyParser = require("body-parser");
-var routes = require("./routes/routes.js");
+var path = require('path');
+var textData;
+
+var config = require("./config.json");
+
 var app = express();
+app.use(cors());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-routes(app);
-
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
+var server = app.listen(config.port, function () {
+    console.log("app running on port:", server.address().port);
 });
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/data', function(req, res) {
+    var text = req.body.text;
+    textData += text;
+    console.log("got post request: "+text);
+    res.send("server got: "+ text);
+});
+
+app.get('/getText', function(req, res) {
+    res.send(textData);
+    textData = "";
+});
+
 
